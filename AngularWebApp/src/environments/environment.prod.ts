d5f8428e7983957144c5a145353cefab684500a1
Adrian Puscasu <apu@f5it.ro>
2019-10-31 23:10:15 +0200

    - work on app.config

--- AngularWebApp/src/environments/environment.prod.ts
+++ AngularWebApp/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: '/api/',
+  version: '0.0.5'
 };
--- AngularWebApp/src/typings.d.ts
+++ AngularWebApp/src/typings.d.ts
@@ -1,2 +1,32 @@
 // so the typescript compiler doesn't complain about the global config object
 declare var config: any;
+
+interface AppConfig {
+  baseUrl?: string;
+  applicationName?: string;
+  emailRegex?: RegExp;
+  version?: string;
+  apiUrl?: string;
+}
+interface IApplicationUser {
+  name?: string;
+  givenName?: string;
+  webSite?: string;
+  address?: IAddress;
+  password?: string;
+  userName?: string;
+  email?: string;
+
+}
+interface IAddress {
+  id?: number;
+  streetAddress?: string;
+  locality?: string;
+  postalCode?: string;
+  country?: ICountry;
+}
+interface ICountry {
+  id?: number;
+  name?: string;
+  abbreviation?: string;
+}
